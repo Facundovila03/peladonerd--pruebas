adcd17a687e0f0b40a1f3d11e0406337b4581790
Facundovila03 <facundovila03@gmail.com>
2025-04-17 17:23:38 -0300

    /app listo, primer parte

--- docker/12/app/src/static/js/app.js
+++ docker/12/app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Items'}
+                        {submitting ? 'Adding...' : 'Add Iteeeeem'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
